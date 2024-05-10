72520515402a8b8e1c1246813d99431e6a06bbda
sean <gsean@copypaste.ph>
2024-05-10 15:46:48 +0800

    Updates on the envoy script and gitlab yml file

--- Scripts/Envoy.blade.php
+++ Scripts/Envoy.blade.php
@@ -1,21 +1,23 @@
 @servers(['staging' => 'staging@724.231.234.123', 'prod' => 'prod@server.hosting.com', 'local' => '127.0.0.1',])
 
 @setup
-  // Make sure to use the SSH clone url
-  $git_repository = 'git@gitlab.com:group/project.git'; // Change This
+  $git_repository = 'git@github.com:project/project.git'; 
+
+  $project_name = 'project';
+  $server_host = $__container->servers[$server];
   
   $release_root = time();
   $releases_dir = [
-    'prod' => '~/project/releases', // Change This
-    'staging' => '~/project/releases', // Change This
+    'prod' => "~/{$project_name}/releases",
+    'staging' => "~/{$project_name}/releases",
   ][$server];
   $shared_dir = [
-    'prod' => '~/project/shared', // Change This
-    'staging' => '~/project/shared', // Change This
+    'prod' => "~/{$project_name}/shared",
+    'staging' => "~/{$project_name}/shared",
   ][$server];
   $release_branch = $branch ?? [
-    'prod' => 'main', // Change this...
-    'staging' => 'develop', // Change this...
+    'prod' => 'main',
+    'staging' => 'develop',
   ][$server];
 
   // Make sure that this paths already exists on the Server
@@ -28,7 +30,7 @@
   $release_lang_custom_path = "$release_path/lang-custom";
   $release_env_path = "$release_path/.env";
   $release_public_path = "$release_path/public";
-  $current_path = '~/minkrystallkule/current_release';
+  $current_path = "~/{$project_name}/current_release";
 
   $serving_path = "~/www/public";
 
@@ -44,6 +46,12 @@
   //
   $spatieMediaLibrary = true;
 @endsetup
+
+@before
+  if (!isset($server)) {
+      throw new Exception("--server option is required");
+  }
+@endbefore
 
 {{-- Servers --}}
 @task('deploy', ['on' => $server])
@@ -114,9 +122,9 @@
     ln -nfs {{ $release_path }} {{ $current_path }}
 
     @if ($cleanup)
-      {{-- Remove Old release but keep three --}}
+      {{-- Remove old release but keep three --}}
       ls -dt {{ "$releases_dir/*" }} | tail -n +{{ is_bool($cleanup) ? 4 : $cleanup }} | xargs -d "\n" rm -rf;
-      {{-- remove old media --}}
+      {{-- Remove old media --}}
       {{-- Remove this if your are not using laravel media-library by spatie --}}
       {{-- php artisan media-library:clean --force --}}
     @endif
@@ -167,4 +175,40 @@
 @task('deploy:quick', ['on' => $server])
   cd {{ $current_path }}
   git pull
+@endtask
+
+@task("database:display", ['on' => $server])
+  cat ~/db.txt
+@endtask
+
+{{-- Make sure to use .pgpass || .my.cnf --}}
+{{-- envoy run database:backup --server=? --user=? --host=? --name=? [--is-postgres] [--clean-up] --}}
+@task("database:backup", ['on' => $server])
+  cd {{$shared_dir}}
+  mkdir -p database-backup
+  cd database-backup
+  @if ($cleanUp)
+      rm -rf ./*.sql
+  @endif
+  @if ($isPostgres)
+    pg_dump --no-privileges --no-owner --clean -U {{ $user ?? $server }} -h {{ $host ?? '127.0.0.1' }} -p 5432 {{ $name ?? $server }} > latest.sql
+  @else
+    mysqldump -u {{ $user ?? $server }} -p -h {{ $host ?? '127.0.0.1' }} -P 3306 {{ $name ?? $server }} > latest.sql
+  @endif
+@endtask
+
+{{-- envoy run database:download --server=? --}}
+@task("database:download", ['on' => 'local'])
+  mkdir -p storage/database-backup/{{$server}}
+  rsync --progress -avzpr {{$server_host}}:{{$shared_dir}}/database-backup/ storage/database-backup/{{$server}}
+@endtask
+
+{{-- Make sure to configure use .pgpass || .my.cnf --}}
+{{-- envoy run database:recover --server=? --db=? --user=? --filename=? [--is-postgres] --}}
+@task("database:recover", ['on' => 'local'])
+  @if ($isPostgres)
+    psql -U {{ $user ?? 'postgres' }} -h 127.0.0.1 -p 5432 -d {{ $db ?? $project_name }} -f storage/database-backup/{{ $server }}/{{ $filename ?? 'latest.sql' }} -q -E
+  @else
+    mysql -h 127.0.0.1 -P 3306 {{ $db ?? $project_name }} < storage/database-backup/{{ $server }}/{{ $filename ?? 'latest.sql' }}
+  @endif
 @endtask
